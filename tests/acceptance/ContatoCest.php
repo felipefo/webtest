4f4f23fecf10229cd7ee462b4ad5cb0e01a70989
felipefo <felipefo@users.noreply.github.com>
2023-05-24 15:39:35 -0300

    waitForElementChange

--- tests/acceptance/ContatoCest.php
+++ tests/acceptance/ContatoCest.php
@@ -6,9 +6,9 @@ class ContatoCest
     {
         $I->amOnPage('https://ifes.edu.br/');
         $I->click('Contato');
-        $I->waitForElement('.elementor-widget-container .elementor-accordion-item');
+        $I->waitForElementChange('.elementor-widget-container .elementor-accordion-item');
         $I->click('.elementor-widget-container .elementor-accordion-item');
-        $I->waitForElement('.elementor-widget-container .elementor-accordion-item-content');
+        $I->waitForElementChange('.elementor-widget-container .elementor-accordion-item-content');
         $telefone = $I->grabTextFrom('.elementor-widget-container .elementor-accordion-item-content .recepcao .telefone');
         $I->assertNotEmpty($telefone);
     }
