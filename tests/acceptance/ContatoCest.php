9d5d05bc42cc08116029cf82b0ed192ce4ff885c
felipefo <felipefo@users.noreply.github.com>
2023-05-25 13:57:44 -0300

    .

--- tests/acceptance/ContatoCest.php
+++ tests/acceptance/ContatoCest.php
@@ -5,16 +5,8 @@ class ContatoCest
     public function verificarTelefoneReitoria(AcceptanceTester $I)
     {
         $I->amOnPage('https://ifes.edu.br/contato');
-        //$I->click('Contato');
-		//$I->seeCurrentURLEquals('/contato');
-		//$I->wait(30);
 		$I->see('Recepção Gabinete do Reitor');
 		$I->see('3357-7500');
-		
-        //$I->waitForElement('.elementor-widget-container .elementor-accordion-item');
-        //$I->click('.elementor-widget-container .elementor-accordion-item');
-        //$I->waitForElement('.elementor-widget-container .elementor-accordion-item-content' ,30);
-        //$telefone = $I->grabTextFrom('.elementor-widget-container .elementor-accordion-item-content .recepcao .telefone');
-        //$I->assertNotEmpty($telefone);
+
     }
 }
