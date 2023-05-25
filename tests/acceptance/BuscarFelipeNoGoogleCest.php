078064570360527ad47054854a777b985fed9d02
felipefo <felipefo@users.noreply.github.com>
2023-05-25 15:06:19 -0300

    Update BuscarFelipeNoGoogleCest.php

--- tests/acceptance/BuscarFelipeNoGoogleCest.php
+++ tests/acceptance/BuscarFelipeNoGoogleCest.php
@@ -4,8 +4,7 @@ class ContatoCest
 {
     public function verificarTelefoneReitoria(AcceptanceTester $I)
     {
-        $I = new AcceptanceTester($scenario);
-		$I->amOnPage('www.google.com');
+       	$I->amOnPage('www.google.com');
 		$I->fillField('q', 'FELIPE FRECHIANI DE OLIVEIRA');
 		$I->amOnPage('/search');
 		$I->click('Felipe Frechiani de Oliveira - Nemo - Ufesufes.brhttps://nemo.inf.ufes.br › equipe › felipe-oliveira');
