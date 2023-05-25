5bb58c2fd9619748f46a9535f3087b2f940942a5
felipefo <felipefo@users.noreply.github.com>
2023-05-25 08:00:47 -0300

    Update ContatoCest.php

--- tests/acceptance/ContatoCest.php
+++ tests/acceptance/ContatoCest.php
@@ -5,11 +5,11 @@ class ContatoCest
     public function verificarTelefoneReitoria(AcceptanceTester $I)
     {
         $I->amOnPage('https://ifes.edu.br/');
-        //$I->click('Contato');
-        //$I->waitForElement('.elementor-widget-container .elementor-accordion-item' , 30);
-        //$I->click('.elementor-widget-container .elementor-accordion-item');
-        //$I->waitForElement('.elementor-widget-container .elementor-accordion-item-content' ,30);
-        //$telefone = $I->grabTextFrom('.elementor-widget-container .elementor-accordion-item-content .recepcao .telefone');
-        //$I->assertNotEmpty($telefone);
+        $I->click('Contato');
+        $I->waitForElement('.elementor-widget-container .elementor-accordion-item' , 30);
+        $I->click('.elementor-widget-container .elementor-accordion-item');
+        $I->waitForElement('.elementor-widget-container .elementor-accordion-item-content' ,30);
+        $telefone = $I->grabTextFrom('.elementor-widget-container .elementor-accordion-item-content .recepcao .telefone');
+        $I->assertNotEmpty($telefone);
     }
 }
